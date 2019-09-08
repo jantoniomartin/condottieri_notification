332b3517f84f78b17ce52036e67518d1cc92865f
José Antonio Martín Prieto <jantonio.martin@gmail.com>
2019-09-08 23:36:30 +0200

    User.is_authenticated is now a property

--- context_processors.py
+++ context_processors.py
@@ -1,7 +1,7 @@
 from condottieri_notification.models import Notice
 
 def notification(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         return {
             "notice_unseen_count": Notice.objects.unseen_count_for(request.user)
         }
