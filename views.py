9707f5128092d4c0d9b2f600cc5998b82dd2c9bd
José Antonio Martín Prieto <jantonio.martin@gmail.com>
2019-09-09 21:06:50 +0200

    removed unused imports

--- views.py
+++ views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 from django.views.generic.base import View
