e49937fb343f812914db6beb156e7f786bea0e35
José Antonio Martín Prieto <jantonio.martin@gmail.com>
2019-09-10 21:27:19 +0200

    fixes for Django 2.0 and pinax-notifications 5.0.3

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView
